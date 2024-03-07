b48fd0251ad4ae5185cdb23e0da3ee67b0e829ac
Shawn <shaochen.li@hotmail.com>
2024-03-07 22:03:26 +1100

    Backup before everyting breaks

--- gravity.js
+++ gravity.js
@@ -1,1 +1,48 @@
+// Initialise gravity
 var gravDir = "down";
+
+// Initialise directions
+
+var DIRECTIONS = {
+    "up" : { "axis" : "y", "sign" : -1 },
+    "down": { "axis" : "y", "sign" : 1 },
+    "left" : { "axis" : "x", "sign" : -1 },
+    "right" : { "axis" : "x", "sign" : 1 },
+}
+
+function changeGrav(gravDir) {
+    switch (gravDir) {
+        case "down":
+            DIRECTIONS = {
+                "up" : { "axis" : "y", "sign" : -1 },
+                "down": { "axis" : "y", "sign" : 1 },
+                "left" : { "axis" : "x", "sign" : -1 },
+                "right" : { "axis" : "x", "sign" : 1 },
+            }
+            break
+        case "up":
+            DIRECTIONS = {
+                "up" : { "axis" : "y", "sign" : 1 },
+                "down": { "axis" : "y", "sign" : -1 },
+                "left" : { "axis" : "x", "sign" : -1 },
+                "right" : { "axis" : "x", "sign" : 1 },
+            }
+            break
+        case "left":
+            DIRECTIONS = {
+                "up" : { "axis" : "x", "sign" : 1 },
+                "down": { "axis" : "x", "sign" : -1 },
+                "left" : { "axis" : "y", "sign" : -1 },
+                "right" : { "axis" : "y", "sign" : 1 },
+            }
+            break
+        case "right":
+            DIRECTIONS = {
+                "up" : { "axis" : "x", "sign" : -1 },
+                "down": { "axis" : "x", "sign" : 1 },
+                "left" : { "axis" : "y", "sign" : 1 },
+                "right" : { "axis" : "y", "sign" : -1 },
+            }
+            break
+    }
+}
--- obstacles.js
+++ obstacles.js
@@ -4,7 +4,7 @@ var player = Bodies.rectangle(640, 0, 40, 40, {friction : 0, frictionAir : 0, fr
 player.timeScale = 1;
 
 var floor = Bodies.rectangle(640, 575, 1280, 30, {isStatic: true});
-var ceiling = Bodies.rectangle(640, 575, 1280, 30, {isStatic: true});
+var ceiling = Bodies.rectangle(640, 0, 1280, 30, {isStatic: true});
 var wallLeft = Bodies.rectangle(0, 287, 30, 575, {isStatic : true});
 var wallRight = Bodies.rectangle(1280, 287, 30, 575, {isStatic : true});
 
