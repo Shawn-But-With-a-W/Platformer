6a9b7f81abc1ec7310652d3744951ed8440a6922
Shawn <shaochen.li@hotmail.com>
2024-04-10 13:25:17 +1000

    Last commit was from the wrong folder

--- controls.js
+++ controls.js
@@ -1,34 +1,44 @@
-// Record of whether a key is currently being pressed
-var keysPressed = {"ArrowUp" : false, "ArrowDown" : false, "ArrowRight" : false, "ArrowLeft" : false};
+// Record of whether a direction key is currently being pressed
+var keysPressed = { "ArrowUp" : false, "ArrowDown" : false,  "ArrowLeft" : false, "ArrowRight" : false };
+var directionsPressed = { "up" : false, "down" : false, "left" : false, "right" : false };
+var gravPressed = { "W" : false, "A" : false, "S" : false, "D" : false };
 
 // Change the key's corresponding value (get it?) to be true when pressed
 window.addEventListener("keydown", (event) => {
     switch (event.key) {
+        // Switch fallthrough (weird syntax I know)
         case "ArrowUp":
-            keysPressed["ArrowUp"] = true;
-            break;
         case "ArrowDown":
-            keysPressed["ArrowDown"] = true;
-            break;
         case "ArrowRight":
-            keysPressed["ArrowRight"] = true;
-            break;
         case "ArrowLeft":
-            keysPressed["ArrowLeft"] = true;
-    }});
+            keysPressed[event.key] = true;
+            directionsPressed[KEYSTROKE_TO_DIRECTION[event.key]] = true;
+            break
+        
+        case "W":
+        case "A":
+        case "S":
+        case "D":
+            gravPressed[event.key] = true;
+            break
+
+        case "w":
+        case "a":
+        case "s":
+        case "d":
+            gravPressed[event.key.toUpperCase()] = true;
+            break
+    }
+});
 
 // Change the key's corresponding value to be false when released
 window.addEventListener("keyup", (event) => {
     switch (event.key) {
         case "ArrowUp":
-            keysPressed["ArrowUp"] = false;
-            break;
         case "ArrowDown":
-            keysPressed["ArrowDown"] = false;
-            break;
         case "ArrowRight":
-            keysPressed["ArrowRight"] = false;
-            break;
         case "ArrowLeft":
-            keysPressed["ArrowLeft"] = false;
+            keysPressed[event.key] = false;
+            directionsPressed[KEYSTROKE_TO_DIRECTION[event.key]] = false;
+            break
     }});
--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-// TODO: add acceleration and deceleration
-// TODO: add obstacles and test
+// TODO: Add level storage
+
+// TODO: Design a level
+
+// TODO: fix switching grav left or right when on down ground
+
+// TODO: Allow to cling on wall even in phase 2, with colour coding working
 
 // module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
     Body = Matter.Body,
     Composite = Matter.Composite,
     Collision = Matter.Collision,
@@ -34,12 +38,6 @@ var render = Render.create({
         showVelocity : true,
 },});
 
-// Create box and ground
-var player = Bodies.rectangle(640, 0, 80, 80, {friction : 0, frictionAir : 0, frictionStatic : 0});
-player.timeScale = 1;
-var ground = Bodies.rectangle(640, 575, 1280, 30, {isStatic: true});
-var collidableBodies = [ground];
-
 // add mouse control
 var mouse = Mouse.create(render.canvas),
     mouseConstraint = MouseConstraint.create(engine, {
@@ -51,39 +49,130 @@ var mouse = Mouse.create(render.canvas),
             }
         }
     });
-
 // keep the mouse in sync with rendering
 render.mouse = mouse;
 
-
 // add all of the bodies to the world
-Composite.add(engine.world, [player, ground, mouseConstraint]);
+Composite.add(engine.world, [player, floor, ceiling, wallLeft, wallRight, mouseConstraint]);
 
 // run the renderer
 Render.run(render);
 
+// Initialise a bunch of variables before the main loop
+var _grav = false;
+var gravTimer = 0;
+var _gravChanged = false;
+var gravRevert = gravDir;
+
 (function mainLoop() {
-    var _onGround = (Collision.collides(player, ground) != null);
+    // Check if it's on the ground
+    var _onGround = false;
+    var type = "air";
+    for (let ground of OBSTACLES.down) {
+        if (Collision.collides(player, ground) != null) {
+            _onGround = true;
+            type = "hor";
+            _grav = true;
+            _gravChanged = false;
+            gravRevert = gravDir;
+            gravTimer = 0;
+            break
+        }
+    }
+
+    // Check if it's on any walls
+
+    var _onLeftWall = false;
+    for (let leftWall of OBSTACLES.left) {
+        if (Collision.collides(player, leftWall) != null) {
+            _onLeftWall = true;
+            break
+            }
+    }
+
+    var _onRightWall = false;
+    for (let rightWall of OBSTACLES.right) {
+        if (Collision.collides(player, rightWall) != null) {
+            _onRightWall = true;
+            break
+            }
+    }
+    
 
     // Move the box according to keyboard inputs
-    if (keysPressed["ArrowUp"] && _onGround) {
+    if (directionsPressed["up"] && _onGround) {
         jump();
     }
-    if (keysPressed["ArrowDown"]) {
+    if (directionsPressed["down"] && !_onGround && !_gravChanged) {
         fastFall();
     }
-    if (keysPressed["ArrowRight"]) {
-        move('right');
+    if (directionsPressed["right"]) {
+        move('right', type);
     }
-    if (keysPressed["ArrowLeft"]) {
-        move('left');
+    if (directionsPressed["left"]) {
+        move('left', type);
+    }
+    
+    if (directionsPressed["up"] && directionsPressed["left"] && _onRightWall) {
+            wallJump("left");
+    }
+    else if (directionsPressed["up"] && directionsPressed["right"] && _onLeftWall) {
+            wallJump("right");
     }
 
-    decel();
-    
-    maxVel();
+    decel(type);
+    maxVel(type);
 
-    
+    if (_grav) {
+        player.render.fillStyle = "#f5d259";
+        if (gravPressed["W"]) {
+            changeGrav("up");
+            _grav = false;
+            _gravChanged = true;
+        }
+        else if (gravPressed["S"]) {
+            changeGrav("down");
+            _grav = false;
+            _gravChanged = true;
+        }
+        else if (gravPressed["A"]) {
+            changeGrav("left");
+            _grav = false;
+            _gravChanged = true;
+        }
+        else if (gravPressed["D"]) {
+            changeGrav("right");
+            _grav = false;
+            _gravChanged = true;
+        }
+    }
+    else {
+        player.render.fillStyle = "#71b0f837";
+    }
+
+    if (_gravChanged) {
+        gravTimer++;
+        player.render.fillStyle = "#71aff8";
+        if (gravTimer >= 20) {
+            changeGrav(gravRevert);
+            gravTimer = 0;
+            _gravChanged = false;
+        }
+
+        if (_onLeftWall) {
+            changeGrav("left");
+            gravTimer = 0;
+            _gravChanged = false;
+        }
+        else if (_onRightWall) {
+            changeGrav("right");
+            gravTimer = 0;
+            _gravChanged = false;
+        }
+    }
+
+    gravPressed = { "W" : false, "A" : false, "S" : false, "D" : false };
+
     window.requestAnimationFrame(mainLoop);
     Engine.update(engine, 1000 / 60);
 })();
