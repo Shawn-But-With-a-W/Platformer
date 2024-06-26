5fc4b4385c820a9043afcf43ec218741989da336
Shawn <shaochen.li@hotmail.com>
2024-06-26 13:30:20 +1000

    The last commit had the wrong files

--- main.js
+++ main.js
@@ -1,217 +1,367 @@
-// TODO: Add Death
+// TODO: fix key buffer after death (it's a feature now)
 
-// TODO: Can only gravity change when one direction key is pressed
-
-// TODO: Make wavedashes non-bufferable
+// TODO: build a new level
 
 // Initialise a bunch of variables before the main loop
 var _grav = false;
-var groundTimer = 0
+var groundTimer = 0;
 var gravTimer = 0;
 var airTimer = 0;
 var _gravChanged = false;
 var _gravReverted = false;
 var gravRevert = gravDir;
-var changeGravDir = "";
+var changeGravDir = null;
+var _isAlive = true;
+var respawnTimer = 0;
+var _waveDash = false;
+var end = false;
+var shakeTimer;
+var _transition = false;
+var transitionTimer = 0;
+var xDisp, yDisp, lmx, lmy, lxx, lxy;
+var timeCurrent = null;
+var timePrev, timeDiff;
+var _boundsSet = false;
 
-(function mainLoop() {
-    // Check if it's on the ground
-    _onGround = isOnGround();
-    _onCeiling = isOnCeiling();
-    _onLeftWall = isOnLeftWall();
-    _onRightWall = isOnRightWall();
+// add mouse control (I have no idea how this works)
+var mouse = Mouse.create(document.body),
+	mouseConstraint = MouseConstraint.create(engine, {
+		mouse: mouse,
+		constraint: {
+			stiffness: 0.2,
+			render: {
+				visible: true,
+			},
+		},
+	});
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+Composite.add(engine.world, [mouseConstraint]);
 
-    var type = "air";
-    if (_onGround  && !_gravChanged && !_gravReverted) {
-        type = "hor";
-        _grav = true;
-        _gravReverted = false;
-        _gravChanged = false;
-        gravTimer = 0;
-        gravRevert = gravDir;
-        changeGravDir = "";
-        groundTimer--;
-    }
+function mainLoop() {
+	// console.log(_boundsSet);
+	// Render.lookAt(render, player, { x: 400, y: 400 });
 
-    if (!_onGround) {
-        airTimer++;
-        groundTimer--;
-    }
-    else {
-        airTimer = 0;
-    }
+	// Bounds.translate(render.bounds, {x:2, y:2});
 
-    for (let spikeObj of SPIKES) {
-        if (spikeObj.hitSpikes()) {
-                console.log("dead");
-        }
-    }
+	// if (player.position.x >= 640) {
+	//     Bounds.shift(render.bounds, {x:640, y:0});
+	// }
+	// else {
+	//     Bounds.shift(render.bounds, {x:-640, y:0})
+	// }
 
-    // Deceleration and capping max velocity
-    if (isNeutral()) {
-        decel(type);
-    }
+	// Check if it's on the ground
+	_onGround = isOnGround();
+	_onCeiling = isOnCeiling();
+	_onLeftWall = isOnLeftWall();
+	_onRightWall = isOnRightWall();
 
-    if (groundTimer < 0) {
-        maxVel(type);
-        PARAMETERS.acc.hor = 0.5;
-        PARAMETERS.acc.air = 0.6;
-    }
-    else {
-        PARAMETERS.acc.hor = 0.1;
-        PARAMETERS.acc.air = 1;
-    }
+	var type = "air";
+	// Reset all variables when landed on ground
+	if (_onGround && !_gravChanged && !_gravReverted) {
+		type = "hor";
+		_grav = true;
+		_gravReverted = false;
+		_gravChanged = false;
+		gravTimer = 0;
+		gravRevert = gravDir;
+		changeGravDir = null;
+		groundTimer--;
+	}
 
-    // Gravity takes priority over other movement
-    if (_grav && !_onGround && airTimer >= 15) {
-        player.render.fillStyle = "#f5d259";
-        if (keysPressed["ArrowLeft"] && _gravPressed) {
-            changeGrav("left");
-            _grav = false;
-            _gravChanged = true;
-            changeGravDir = "left";
-            engine.gravity.scale = 0.002;
-        }
-        else if (keysPressed["ArrowRight"] && _gravPressed) {
-            changeGrav("right");
-            _grav = false;
-            _gravChanged = true;
-            changeGravDir = "right";
-            engine.gravity.scale = 0.002;
+	// Start air timer when in air
+	if (!_onGround) {
+		airTimer++;
+		groundTimer--;
+	} else {
+		airTimer = 0;
+	}
 
-        }
-        else if (keysPressed["ArrowUp"] && _gravPressed) {
-            changeGrav("up");
-            _grav = false;
-            _gravChanged = true;
-            changeGravDir = "up";
-            engine.gravity.scale = 0.002;
+	// Dumb ways to die (and some less dumb ones)
+	if (_isAlive) {
+		respawnTimer = 0;
+		// Hitting a spike
+		for (const spikeObj of SPIKES) {
+			if (spikeObj.hitSpikes()) {
+				death();
+			}
+		}
 
-        }
-        else if (keysPressed["ArrowDown"] && _gravPressed) {
-            changeGrav("down");
-            _grav = false;
-            _gravChanged = true;
-            changeGravDir = "down";
-            engine.gravity.scale = 0.002;
-        }
-    }
-    else {
-        player.render.fillStyle = "#f5d35954";
-    }
+		for (const fallSpikeObj of FALLING_SPIKES[levelIndex]) {
+			if (fallSpikeObj.hitSpikes()) {
+				death();
+			}
+		}
 
+		// Falling out of bounds
+		if (currentLevel.isOutOfBounds()) {
+			death();
+		}
+	} else {
+		setBounds();
 
-    // Move the box according to keyboard inputs
-    if (directionsPressed["up"] && _onGround) {
-        jump();
-    }
-    if (directionsPressed["down"] && !_onGround && !_gravChanged ) {
-        fastFall();
-    }
-    if (directionsPressed["right"]) {
-        move('right', type);
-    }
-    if (directionsPressed["left"]) {
-        move('left', type);
-    }
+		screenShakeDeath(respawnTimer, "death");
+		respawnTimer++;
+		// Respawn after set amount of time
+		if (respawnTimer >= 100) {
+			respawn(currentLevel.spawn);
+			respawnTimer = 0;
+		}
+		// Change speed of death animation depending on amount of time after death
+		else if (respawnTimer >= 30) {
+			engine.timing.timeScale = 1;
+		} else if (respawnTimer >= 15) {
+			engine.timing.timeScale = 0.75;
+		}
+	}
 
-    if (directionsPressed["up"] && directionsPressed["left"] && _onRightWall) {
-        wallJump("left");
-    }
-    else if (directionsPressed["up"] && directionsPressed["right"] && _onLeftWall) {
-        wallJump("right");
-    }
+	// Deceleration and capping max velocity
+	if (isNeutral()) {
+		decel(type);
+	}
 
-    if (_gravChanged) {
-        gravTimer++;
-        player.render.fillStyle = "#71aff8";
+	// When on ground for long enough
+	if (groundTimer < 0) {
+		maxVel(type);
+		_waveDash = false;
+		PARAMETERS.acc.hor = 0.5;
+		PARAMETERS.acc.air = 0.6;
+	}
+	// Modifying values for wavedashing for short period of time on ground
+	else {
+		if (directionsPressed["up"] == false) {
+			_waveDash = true;
+		}
 
-        switch (changeGravDir) {
-            case "up":
-            case "down":
-                cancelVel("x");
-                break
-            case "left":
-            case "right":
-                cancelVel("y");
-                break
-        }
+		if (_waveDash) {
+			PARAMETERS.acc.hor = 0.1;
+			PARAMETERS.acc.air = 1;
+		} else {
+			maxVel(type);
+		}
+	}
 
-        if (gravTimer >= 10) {
-            changeGrav(gravRevert);
-            _gravReverted = true;
-            _gravChanged = false;
-        }
+	// Gravity takes priority over other movement
+	if (_grav && !_onGround && airTimer >= 15) {
+		player.render.fillStyle = "#f5d259";
+		if (
+			keysPressed["ArrowLeft"] &&
+			_gravPressed &&
+			!(keysPressed["ArrowRight"] || keysPressed["ArrowUp"] || keysPressed["ArrowDown"])
+		) {
+			changeGrav("left");
+			_grav = false;
+			_gravChanged = true;
+			changeGravDir = "left";
+			engine.gravity.scale = 0.002;
+		} else if (
+			keysPressed["ArrowRight"] &&
+			_gravPressed &&
+			!(keysPressed["ArrowLeft"] || keysPressed["ArrowUp"] || keysPressed["ArrowDown"])
+		) {
+			changeGrav("right");
+			_grav = false;
+			_gravChanged = true;
+			changeGravDir = "right";
+			engine.gravity.scale = 0.002;
+		} else if (
+			keysPressed["ArrowUp"] &&
+			_gravPressed &&
+			!(keysPressed["ArrowRight"] || keysPressed["ArrowLeft"] || keysPressed["ArrowDown"])
+		) {
+			changeGrav("up");
+			_grav = false;
+			_gravChanged = true;
+			changeGravDir = "up";
+			engine.gravity.scale = 0.002;
+		} else if (
+			keysPressed["ArrowDown"] &&
+			_gravPressed &&
+			!(keysPressed["ArrowRight"] || keysPressed["ArrowUp"] || keysPressed["ArrowLeft"])
+		) {
+			changeGrav("down");
+			_grav = false;
+			_gravChanged = true;
+			changeGravDir = "down";
+			engine.gravity.scale = 0.002;
+		}
+	} else {
+		player.render.fillStyle = "#f5d3598f";
+	}
 
-        // Sticking to the direction gravity changed in while going upwards
-        if (_onGround && gravTimer >= 3) {
-            gravRevert = gravDir;
-            changeGrav(gravRevert);
-            _gravChanged = false;
-            _gravReverted = false;
-            gravTimer = 0;
-            neutral();
-        }
-        
-    }
-    else {
-        engine.gravity.scale = 0.001;
-    }
+	// Move the box according to keyboard inputs
+	if (directionsPressed["up"] && _onGround) {
+		jump();
+	}
+	if (directionsPressed["down"] && !_onGround && !_gravChanged) {
+		fastFall();
+	}
+	if (directionsPressed["right"]) {
+		move("right", type);
+	}
+	if (directionsPressed["left"]) {
+		move("left", type);
+	}
 
-    if (_gravReverted) {
-        player.render.fillStyle = "#71b0f837";
-        
-        // Sticking to the direction gravity changed in while falling back down
-        switch (changeGravDir) {
-            case "up":
-                if (isOnUpObst()) {
-                    changeGrav("up");
-                    _gravReverted  = false;
-                    neutral();
-                }
-                break
-            case "down":
-                if (isOnDownObst()) {
-                    changeGrav("down");
-                    _gravReverted  = false;
-                    neutral();
-                }
-                break
-            case "left":
-                if (isOnLeftObst()) {
-                    changeGrav("left");
-                    _gravReverted  = false;
-                    neutral();
-                }
-                break
-            case "right":
-                if (isOnRightObst()) {
-                    changeGrav("right");
-                    _gravReverted  = false;
-                    neutral();
-                }
-                break
-        }
+	if (directionsPressed["up"] && directionsPressed["left"] && _onRightWall) {
+		wallJump("left");
+	} else if (directionsPressed["up"] && directionsPressed["right"] && _onLeftWall) {
+		wallJump("right");
+	}
 
-        for (let key of Object.keys(keysPressed)) {
-            if (!keysPressed[key]) {
-                directionsPressed[KEYSTROKE_TO_DIRECTION[key]] = false;
-            }
-        }
+	if (_gravChanged) {
+		gravTimer++;
+		player.render.fillStyle = "#71aff8";
 
-        if (_onGround) {
-            type = "hor";
-            _grav = true;
-            _gravReverted = false;
-            _gravChanged = false;
-            gravTimer = 0;
-            gravRevert = gravDir;
-            changeGravDir = "";
-            groundTimer = 11;
-        }
-    }
-    
+		// Cancel velocity on secondary axis to not move diagonally
+		switch (changeGravDir) {
+			case "up":
+			case "down":
+				cancelVel("x");
+				break;
+			case "left":
+			case "right":
+				cancelVel("y");
+				break;
+		}
 
-    window.requestAnimationFrame(mainLoop);
-    Engine.update(engine, 1000 / 60);
-    })();
+		// Switching gravity back after 10 ticks
+		if (gravTimer >= 10) {
+			changeGrav(gravRevert);
+			_gravReverted = true;
+			_gravChanged = false;
+		}
+
+		// Sticking to the direction gravity changed in while going upwards
+		if (_onGround && gravTimer >= 3) {
+			gravRevert = gravDir;
+			changeGrav(gravRevert);
+			_gravChanged = false;
+			_gravReverted = false;
+			gravTimer = 0;
+			neutral();
+		}
+	} else {
+		engine.gravity.scale = 0.001;
+	}
+
+	if (_gravReverted) {
+		player.render.fillStyle = "#71b0f837";
+
+		// Still sticking to the direction gravity previously changed in after revert
+		switch (changeGravDir) {
+			case "up":
+				if (isOnUpObst()) {
+					changeGrav("up");
+					_gravReverted = false;
+					neutral();
+				}
+				break;
+			case "down":
+				if (isOnDownObst()) {
+					changeGrav("down");
+					_gravReverted = false;
+					neutral();
+				}
+				break;
+			case "left":
+				if (isOnLeftObst()) {
+					changeGrav("left");
+					_gravReverted = false;
+					neutral();
+				}
+				break;
+			case "right":
+				if (isOnRightObst()) {
+					changeGrav("right");
+					_gravReverted = false;
+					neutral();
+				}
+				break;
+		}
+
+		// Updating keysPressed record so keys/directions aren't incorrectly buffered after gravity change
+		for (const key of Object.keys(keysPressed)) {
+			if (!keysPressed[key]) {
+				directionsPressed[KEYSTROKE_TO_DIRECTION[key]] = false;
+			}
+		}
+
+		// Resetting values specifically after gravity change
+		if (_onGround) {
+			type = "hor";
+			_grav = true;
+			_gravReverted = false;
+			_gravChanged = false;
+			gravTimer = 0;
+			gravRevert = gravDir;
+			changeGravDir = null;
+			groundTimer = 17;
+		}
+	}
+
+	const endObj = currentLevel.checkLevelComplete();
+	if (endObj !== null) {
+		for (const fallPlat of FALLING_PLATFORMS[levelIndex]) {
+			fallPlat.reset();
+			Sleeping.set(fallPlat.platform, true);
+		}
+
+		for (const fallSpike of FALLING_SPIKES[levelIndex]) {
+			fallSpike.reset();
+			Sleeping.set(fallSpike.spike, true);
+		}
+
+		// console.log(endObj.range);
+		// endObj.range.remove();
+		levelIndex = currentLevel.nextLevelIndex(endObj);
+		currentLevel = currentLevel.nextLevel(endObj);
+		_transition = true;
+		transitionTimer = 0;
+
+		for (const fallPlat of FALLING_PLATFORMS[levelIndex]) {
+			// fallPlat.reset();
+			Sleeping.set(fallPlat.platform, false);
+		}
+
+		for (const fallSpike of FALLING_SPIKES[levelIndex]) {
+			// fallSpike.reset();
+			Sleeping.set(fallSpike.spike, false);
+		}
+
+		setBounds();
+		xDisp = currentLevel.max.x - lxx;
+		yDisp = currentLevel.max.y - lxy;
+	}
+
+	if (_transition) {
+		engine.timing.timeScale = 0;
+		tween(transitionTimer, 40, xDisp, yDisp, easeInOutSine);
+		transitionTimer++;
+
+		if (transitionTimer >= 42) {
+			_transition = false;
+			engine.timing.timeScale = 1;
+			console.log("switched rooms", _boundsSet);
+			_grav = true;
+			_gravChanged = false;
+			_gravReverted = false;
+		}
+	}
+
+	// Allow other viewport animations to take priority
+	if (_isAlive && !_transition && shakeTimer <= 40) {
+		screenShakeGrav(shakeTimer, changeGravDir);
+		shakeTimer++;
+	}
+
+	timePrev = timeCurrent;
+	timeCurrent = new Date().getTime();
+	timeDiff = timeCurrent - timePrev;
+	timeDiff = typeof timePrev === "number" ? timeCurrent - timePrev : 1000 / 60;
+
+	Engine.update(engine, timeDiff);
+	requestAnimationFrame(mainLoop);
+}
+requestAnimationFrame(mainLoop);
--- window.js
+++ window.js
@@ -1,36 +1,131 @@
-// module aliases
+// Module aliases
 var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Body = Matter.Body,
-    Composite = Matter.Composite,
-    Collision = Matter.Collision,
-    Mouse = Matter.Mouse,
-    MouseConstraint = Matter.MouseConstraint;
+	Render = Matter.Render,
+	Runner = Matter.Runner,
+	Body = Matter.Body,
+	Bounds = Matter.Bounds,
+	Composite = Matter.Composite,
+	Collision = Matter.Collision,
+	Mouse = Matter.Mouse,
+	MouseConstraint = Matter.MouseConstraint,
+	Events = Matter.Events,
+	Sleeping = Matter.Sleeping;
 
-
-// create an engine
+// Create an engine
 var engine = Engine.create();
 
-// create a renderer
+// Create a renderer
 var render = Render.create({
-    canvas : document.getElementById("game-screen"),
-    engine : engine,
-    options : {
-        width : 1280, 
-        height : 650, 
-        background : "#c2c2c2", 
-        pixelRatio : 'auto',
-        wireframes : false,
-        showAngleIndicator : true,
-        showAxes : true,
-        showBounds : true,
-        showCollisions : true,
-        showDebug : true,
-        showIds : true,
-        showPositions : true,
-        showVelocity : true,
-},});
+	canvas: document.getElementById("game-screen"),
+	engine: engine,
+	options: {
+		width: 1280,
+		height: 650,
+		background: "#b5b5b5",
+		pixelRatio: "auto",
+		wireframes: false,
+		hasBounds: true,
+		showDebug: true,
+		// showAngleIndicator : true,
+		// showAxes : true,
+		// showBounds : true,
+		// showCollisions : true,
+		// showIds : true,
+		// showPositions : true,
+		// showVelocity : true,
+	},
+});
 
-// run the renderer
+// Run the renderer
 Render.run(render);
+
+function screenShakeDeath(t) {
+	if (t <= 30) {
+		Bounds.translate(render.bounds, {
+			x: ((30 - t) / 4) * Math.sin(((2 * Math.PI) / 7) * t),
+			y: ((30 - t) / 4) * Math.sin(((2 * Math.PI) / 7) * (t - 1)),
+		});
+	} else {
+		tween(t - 30, 2, currentLevel.max.x - render.bounds.max.x, currentLevel.max.y - render.bounds.max.y);
+
+		// render.bounds.min.x = currentLevel.min.x;
+		// render.bounds.min.y = currentLevel.min.y;
+		// render.bounds.max.x = currentLevel.max.x;
+		// render.bounds.max.y = currentLevel.max.y;
+	}
+}
+
+function screenShakeGrav(t, dir) {
+	if (t <= 2) {
+		switch (dir) {
+			case "up":
+				Bounds.translate(render.bounds, {
+					x: 0,
+					y: -0.5,
+				});
+				break;
+			case "down":
+				Bounds.translate(render.bounds, {
+					x: 0,
+					y: 0.5,
+				});
+				break;
+			case "left":
+				Bounds.translate(render.bounds, {
+					x: -0.5,
+					y: 0,
+				});
+				break;
+			case "right":
+				Bounds.translate(render.bounds, {
+					x: 0.5,
+					y: 0,
+				});
+				break;
+		}
+	} else {
+		if (!_boundsSet) {
+			setBounds();
+		}
+		// console.log(currentLevel.max.x - lxx, currentLevel.max.y - lxy);
+		// console.log("I was here");
+		// console.log(currentLevel.max.x, currentLevel.max.y);
+		console.log(_boundsSet, lxx, lxy, currentLevel.max.x, currentLevel.max.y);
+		tween(t - 8, 4, currentLevel.max.x - lxx, currentLevel.max.y - lxy);
+	}
+}
+
+function tween(t, tMax, xDisp, yDisp, fn) {
+	// Jarrett wrote this
+	// console.log(t, tMax);
+	if (t <= tMax) {
+		const pos = typeof fn === "function" ? fn(t / tMax) : t / tMax;
+		render.bounds.min.x = lmx + xDisp * pos;
+		render.bounds.min.y = lmy + yDisp * pos;
+		render.bounds.max.x = lxx + xDisp * pos;
+		render.bounds.max.y = lxy + yDisp * pos;
+		// console.log(xDisp, yDisp);
+		// console.log(render.bounds);
+	} else {
+		render.bounds.min.x = currentLevel.min.x;
+		render.bounds.min.y = currentLevel.min.y;
+		render.bounds.max.x = currentLevel.max.x;
+		render.bounds.max.y = currentLevel.max.y;
+		_boundsSet = false;
+		console.log(_boundsSet);
+		// setBounds();
+	}
+}
+
+function setBounds() {
+	({
+		min: { x: lmx, y: lmy },
+		max: { x: lxx, y: lxy },
+	} = render.bounds);
+	_boundsSet = true;
+	// console.log("bound set");
+}
+
+function easeInOutSine(t) {
+	return -(Math.cos(Math.PI * t) - 1) / 2;
+}
